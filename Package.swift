dab041bd7bc55f6bc6c8e88d7254aa5eafce6482
Germain <germain@souquet.com>
2023-02-07 10:51:10 +0000

    Add swift package entry

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "CompoundDesignTokens",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(name: "CompoundDesignTokens", targets: ["CompoundDesignTokens"])
+    ],
+    targets: [
+        .target(name: "CompoundDesignTokens", path: "assets/ios/swift")
+    ]
+)
