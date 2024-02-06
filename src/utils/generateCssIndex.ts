3d4bb2bbb22ace04f6ba0f9f7bb423200a9e030e
Robin <robin@robin.town>
2024-02-06 18:23:01 -0500

    Add basic support for theming on Web
    This adds basic support for overriding semantic tokens on Web via a CSS cascade layer, for which it was necessary to separate base and semantic tokens into different sets of files. Documentation for this theming mechanism will be provided over at the Compound Storybook repository, but it can be used like so:
    
    @import "@vector-im/compound-design-tokens/assets/web/css/compound-design-tokens.css" layer(compound);
    
    @layer compound.custom {
      :root, [class*="cpd-theme-"] {
        --cpd-color-icon-accent-tertiary: var(--cpd-color-blue-800);
      }
    }

---
+++ src/utils/generateCssIndex.ts
@@ -0,0 +1,49 @@
+/*
+Copyright 2024 New Vector Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import fs from 'fs-extra'
+import path from 'path'
+import { Theme } from '../@types'
+import { cssFileName, Tier } from './cssFileName'
+
+const header = `/* Establish a layer order that allows semantic tokens to be customized, but not base tokens */
+@layer semantic, custom, base;`
+
+const themes: (Theme | null)[] = [null, 'light', 'light-hc', 'dark', 'dark-hc']
+const tiers: Tier[] = ['base', 'semantic']
+
+export function generateCssIndex(): void {
+  const imports = [...(function* () {
+    for (const theme of themes) {
+      for (const tier of tiers) {
+        for (const mq of theme === null ? [false] : [false, true]) {
+          let mediaQuery = 'screen'
+          if (mq) {
+            mediaQuery += ` and (prefers-color-scheme: ${theme!.includes('light') ? 'light' : 'dark'})`
+            if (theme!.includes('-hc')) mediaQuery += ` and (prefers-contrast: more)`
+          }
+          yield `@import url("./${cssFileName(theme, tier, mq)}") layer(${tier}) ${mediaQuery};`
+        }
+      }
+    }
+  })()]
+
+  fs.writeFileSync(
+    path.join('assets', 'web', 'css', 'compound-design-tokens.css'),
+    `${header}\n\n${imports.join('\n')}\n`,
+    'utf-8',
+  )
+}
