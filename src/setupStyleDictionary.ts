e910f084333881511ccfc874fedaf5dba077a0c6
Quentin Gliech <quenting@element.io>
2024-06-20 13:43:45 +0200

    Upgrade all packages

--- src/setupStyleDictionary.ts
+++ src/setupStyleDictionary.ts
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
- 
+
 import { registerTransforms } from "@tokens-studio/sd-transforms";
 import StyleDictionary from "style-dictionary";
 import type { Core } from "style-dictionary";
