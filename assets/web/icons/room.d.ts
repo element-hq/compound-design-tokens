fd3c8d823b7836023bdaf2f781439588cdd636ec
Doug <douglase@element.io>
2024-07-24 14:37:49 +0100

    Generate assets.

---
+++ assets/web/icons/room.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * room.svg
+ */
+declare const RoomIcon: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, "ref" | "children"> &
+    React.RefAttributes<SVGSVGElement>
+>;
+
+export default RoomIcon;
