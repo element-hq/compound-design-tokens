c7e4be49a302b48ed59dd70185e2dd880e0e2fe1
Will Hunt <will@half-shot.uk>
2024-11-04 14:37:43 +0000

    Add hand-raised icon.

---
+++ assets/web/icons/hand-raised.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * hand-raised.svg
+ */
+declare const HandRaisedIcon: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, "ref" | "children"> &
+    React.RefAttributes<SVGSVGElement>
+>;
+
+export default HandRaisedIcon;
