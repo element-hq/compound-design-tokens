233edcf55db305e2f224d202225cdf847a4f9f8b
Doug <6060466+pixlwave@users.noreply.github.com>
2024-12-12 12:32:26 +0000

    Add new icons (renames voice-call to voice-call solid) (#133)

---
+++ assets/web/icons/list-view.cjs
@@ -0,0 +1,25 @@
+var _reactJsxRuntime = require("react/jsx-runtime");
+var React = require("react");
+function ListViewIcon(props, ref) {
+  return /*#__PURE__*/_reactJsxRuntime.jsxs("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "1em",
+    height: "1em",
+    fill: "currentColor",
+    viewBox: "0 0 24 24",
+    ref: ref,
+    ...props,
+    children: [/*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      d: "M17 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4Z"
+    }), /*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      fillRule: "evenodd",
+      d: "M4 7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H4Zm8 2H4v6h8V9Z",
+      clipRule: "evenodd"
+    }), /*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      d: "M16 10a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1Zm0 4a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4Z"
+    })]
+  });
+}
+;
+ListViewIcon.displayName = "ListViewIcon";
+module.exports = React.forwardRef(ListViewIcon);
---
+++ assets/web/icons/spotlight-view.cjs
@@ -0,0 +1,21 @@
+var _reactJsxRuntime = require("react/jsx-runtime");
+var React = require("react");
+function SpotlightViewIcon(props, ref) {
+  return /*#__PURE__*/_reactJsxRuntime.jsx("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "1em",
+    height: "1em",
+    fill: "currentColor",
+    viewBox: "0 0 24 24",
+    ref: ref,
+    ...props,
+    children: /*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      fillRule: "evenodd",
+      d: "M20 6H4v12h16V6ZM4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Z",
+      clipRule: "evenodd"
+    })
+  });
+}
+;
+SpotlightViewIcon.displayName = "SpotlightViewIcon";
+module.exports = React.forwardRef(SpotlightViewIcon);
