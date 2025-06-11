e772ffe7c1b7030131114c556d0eec2d09fa9824
robintown <48614497+robintown@users.noreply.github.com>
2025-06-11 02:54:30 +0000

    Sync icons from Figma

--- assets/web/icons/guest.cjs
+++ assets/web/icons/guest.cjs
@@ -1,7 +1,7 @@
 var _reactJsxRuntime = require("react/jsx-runtime");
 var React = require("react");
 function GuestIcon(props, ref) {
-  return /*#__PURE__*/_reactJsxRuntime.jsx("svg", {
+  return /*#__PURE__*/_reactJsxRuntime.jsxs("svg", {
     xmlns: "http://www.w3.org/2000/svg",
     width: "1em",
     height: "1em",
@@ -9,11 +9,11 @@ function GuestIcon(props, ref) {
     viewBox: "0 0 24 24",
     ref: ref,
     ...props,
-    children: /*#__PURE__*/_reactJsxRuntime.jsx("path", {
-      fillRule: "evenodd",
-      d: "M1.304 12.617a1.04 1.04 0 0 1 0-1.47l6.844-6.843a1.04 1.04 0 0 1 1.47 0l1.343 1.344-5.5 5.5a1.04 1.04 0 0 0 0 1.47l5.5 5.5-1.344 1.343a1.04 1.04 0 0 1-1.47 0zm6.235-1.47a1.04 1.04 0 0 0 0 1.47l6.844 6.844a1.04 1.04 0 0 0 1.47 0l6.843-6.843a1.04 1.04 0 0 0 0-1.47l-6.844-6.844a1.04 1.04 0 0 0-1.47 0z",
-      clipRule: "evenodd"
-    })
+    children: [/*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      d: "M8.148 4.304a1.04 1.04 0 0 1 1.47 0l1.343 1.344-5.5 5.5a1.04 1.04 0 0 0 0 1.47l5.5 5.5-1.344 1.343a1.04 1.04 0 0 1-1.47 0l-6.843-6.843a1.04 1.04 0 0 1 0-1.47z"
+    }), /*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      d: "M14.383 4.304a1.04 1.04 0 0 1 1.469 0l6.844 6.844a1.04 1.04 0 0 1 0 1.47l-6.844 6.843a1.04 1.04 0 0 1-1.469 0L7.54 12.618a1.04 1.04 0 0 1 0-1.47z"
+    })]
   });
 }
 ;
