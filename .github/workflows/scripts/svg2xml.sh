ec6233625fe9b7f5f3cf922ea16fddf1a6004776
Jorge Martín <jorgem@element.io>
2025-10-09 14:25:50 +0200

    Try using svg2xml v1.0.2 for batch conversions

--- .github/workflows/scripts/svg2xml.sh
+++ .github/workflows/scripts/svg2xml.sh
@@ -8,23 +8,9 @@ curl -L "https://github.com/element-hq/compound-svg2xml/releases/download/v$1/sv
 unzip svg2xml.zip
 chmod +x svg2xml-$1/bin/svg2xml
 
-snake_case() {
-	# Find the 'words' (groups of either letters or numbers)
-	words=($(echo $1 | grep -oE '[a-zA-Z]+|[0-9]+'))
-	# Then join them with `_`
-	echo $(IFS=_; echo "${words[*]}")
-}
-
-for icon in $(ls icons/*.svg); do
-	echo "Transforming $icon to Android XML format"
-	# Remove the dir part
-	dest=${icon/icons\//}
-	# Remove extension
-	dest=${dest/.svg/}
-	# Make sure it's snake case
-	dest=$(snake_case $dest)
-	svg2xml-$1/bin/svg2xml $icon assets/android/res/drawable/ic_compound_$dest.xml
-done
+# Perform bathc conversion
+echo "Converting Android icons from SVG to XML..."
+svg2xml-$1/bin/svg2xml icons assets/android/res/drawable/ ic_compound
 
 # Do some cleanup
 rm -r svg2xml*
