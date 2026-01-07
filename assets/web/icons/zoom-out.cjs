d811de3ce90eb3ac6e12cbf5ce29232b6d4d8803
t3chguy <2403652+t3chguy@users.noreply.github.com>
2026-01-07 11:08:02 +0000

    Sync icons from Figma

--- assets/web/icons/zoom-out.cjs
+++ assets/web/icons/zoom-out.cjs
@@ -9,22 +9,12 @@ function ZoomOutIcon(props, ref) {
     viewBox: "0 0 24 24",
     ref: ref,
     ...props,
-    children: [/*#__PURE__*/_reactJsxRuntime.jsxs("g", {
-      clipPath: "url(#a)",
-      children: [/*#__PURE__*/_reactJsxRuntime.jsx("path", {
-        d: "M13.5 9.5q.425 0 .713.287.288.288.287.713a.97.97 0 0 1-.287.713.97.97 0 0 1-.713.287h-6a.97.97 0 0 1-.713-.287.97.97 0 0 1-.287-.713q0-.425.287-.713A.97.97 0 0 1 7.5 9.5z"
-      }), /*#__PURE__*/_reactJsxRuntime.jsx("path", {
-        fillRule: "evenodd",
-        d: "M10.5 3a7.5 7.5 0 0 1 5.963 12.049l3.244 3.244a1 1 0 1 1-1.414 1.414l-3.244-3.244A7.5 7.5 0 1 1 10.5 3m0 2a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11",
-        clipRule: "evenodd"
-      })]
-    }), /*#__PURE__*/_reactJsxRuntime.jsx("defs", {
-      children: /*#__PURE__*/_reactJsxRuntime.jsx("clipPath", {
-        id: "a",
-        children: /*#__PURE__*/_reactJsxRuntime.jsx("path", {
-          d: "M0 0h24v24H0z"
-        })
-      })
+    children: [/*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      d: "M13.5 9.5q.425 0 .713.287.288.288.287.713a.97.97 0 0 1-.287.713.97.97 0 0 1-.713.287h-6a.97.97 0 0 1-.713-.287.97.97 0 0 1-.287-.713q0-.425.287-.713A.97.97 0 0 1 7.5 9.5z"
+    }), /*#__PURE__*/_reactJsxRuntime.jsx("path", {
+      fillRule: "evenodd",
+      d: "M10.5 3a7.5 7.5 0 0 1 5.963 12.049l3.244 3.244a1 1 0 1 1-1.414 1.414l-3.244-3.244A7.5 7.5 0 1 1 10.5 3m0 2a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11",
+      clipRule: "evenodd"
     })]
   });
 }
